558ba0a42bd8435e65e0112a61ed09fd85c8c1a5
Sam <mail.song.de.qiang@gmail.com>
2014-10-29 10:01:12 +0800

    init

--- common/config/main.php
+++ common/config/main.php
@@ -4,7 +4,7 @@ return [
     'components' => [
     	'db' => [
 			'class' => 'yii\db\Connection',
-			'dsn' => 'mysql:host=localhost;dbname=yii_yiii2a',
+			'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
 			'username' => 'root',
 			'password' => '',
 			'charset' => 'utf8'
